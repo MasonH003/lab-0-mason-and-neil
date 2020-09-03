d1e66ecd64cfee51f95a25cfe6fc7a1de1ae2657
Mason <MasonH003@gmail.com>
2020-09-03 10:28:14 -0400

    Added intro comments and one statement.

---
+++ Lab-0-mason-and-neil.py
@@ -0,0 +1,6 @@
+# Team Names: Mason Hall and Neil Keohane
+# Course: CS151, Dr. Rajeev
+# Date: 9/6/2020
+# Lab Number: 0
+
+print(" Hello, my name is Mason. This is the first program that I've ever written.")
