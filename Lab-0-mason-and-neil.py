4b8f706e5d9cf328fcad7e1ab8fb4f77fbdfc49b
Mason <MasonH003@gmail.com>
2020-09-03 10:38:28 -0400

    added 1 statement

--- Lab-0-mason-and-neil.py
+++ Lab-0-mason-and-neil.py
@@ -3,4 +3,4 @@
 # Date: 9/6/2020
 # Lab Number: 0
 
-print(" Hello, my name is Mason. This is the first program that I've ever written.")
+print("Hello, my name is Mason. This is the first program that I've ever written.")
